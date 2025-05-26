f63c3bc107173b53ec57084d978e955980624ffd
diegodcampos <diego.arruda@sptech.school>
2025-05-26 19:37:49 -0300

    Second commit

---
+++ src/main/java/com/example/atividadeCICD/AtividadeCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividadeCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atividadeCICD/AtividadeCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadeCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
